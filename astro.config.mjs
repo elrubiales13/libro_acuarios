d0f7b07f85c06cd9d7811a01356eb940fada8b6e
elrubiales13 <93713849+elrubiales13@users.noreply.github.com>
2026-01-20 19:46:37 +0100

    Remove Node adapter and set output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://elrubiales13.github.io',
   base: '/libro_acuarios/',
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'static'
 });
